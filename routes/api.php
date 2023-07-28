bac7421750ec606a36bc41936a1fa8389a605858
LastOfWhom <sharifullinbulat19349Gmail.com>
2023-07-28 16:12:17 +0300

    make jwt token

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group([
 
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
+
+Route::middleware('jwt.auth')->group(function (){
+    Route::get('/posts', [App\Http\Controllers\Post\GetPostController::class, 'getAllPosts']);
+});
